ff0ad6f9688e7a619a0d19cc7c2f961fd7c21f9c
Aleksandra Pstrong <olapstrong@gmail.com>
2024-05-05 16:12:19 +0200

    fixed nav bar for big screen, fixed button for small screen , updated home page and global css

--- VueProject/quizloFrontend/src/components/Nav.vue
+++ VueProject/quizloFrontend/src/components/Nav.vue
@@ -7,11 +7,12 @@
         <!-- Small logo for tablet and phone -->
         <img v-else src="../images/quizlo-logo.png" id="quizlo-logo" alt="Quizlo Logo" style="width: 60px; height: auto;">
       </router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" @click="toggleNavbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <div class="navbar-nav ml-auto">
+      <div :class="['collapse', 'navbar-collapse', { 'show': isNavbarOpen }]" id="navbarNav">
+
+      <div class="navbar-nav ms-auto">
           <router-link  to="/" class="btn btn-light mx-2 custom-btn" style="background-color: #60A1BC; color: white; border: none;">Home Page</router-link>
           <routerLink to="/aboutUs" class="btn btn-light mx-2 custom-btn" style="background-color: #60A1BC; color: white; border: none;">About Us</routerLink>
           <router-link to="/login" class="btn btn-light mx-2 custom-btn" style="background-color: #60A1BC; color: white; border: none;">Log in / Register</router-link>
@@ -24,12 +25,17 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, computed } from 'vue';
 
 const isDesktop = ref(window.innerWidth >= 992);
+const isNavbarOpen = ref(false);
 const darkMode = ref(localStorage.getItem('darkMode') === 'true');
 
 const darkModeText = computed(() => darkMode.value ? 'Light Mode' : 'Dark Mode');
+
+const toggleNavbar = () => {
+  isNavbarOpen.value = !isNavbarOpen.value;
+};
 
 const toggleDarkMode = () => {
   darkMode.value = !darkMode.value;
@@ -46,15 +52,28 @@ const setDarkMode = () => {
   }
 };
 
-onMounted(() => {
-  window.addEventListener('resize', () => {
-    isDesktop.value = window.innerWidth >= 992;
-  });
-
-  setDarkMode();
+window.addEventListener('resize', () => {
+  isDesktop.value = window.innerWidth >= 992;
 });
+
+setDarkMode();
 </script>
 
 <style scoped>
-/* Add your styles here */
+@media (max-width: 991.98px) {
+  .navbar-nav.ms-auto {
+    margin-top: 40px; /* Adjust the margin-top value as needed */
+  }
+}
+.btn-outline-light:hover {
+  background-color: #fff;
+  color: #3C89AA;
+}
+.custom-btn {
+  transition: transform 0.3s ease;
+}
+.custom-btn:hover {
+  transform: scale(1.1);
+}
+
 </style>
