8ce11312ac3c5dd0be92294abe0aea734f4b151f
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-05-14 14:04:38 +0200

    blocked resolving quiz for non logged in users

--- VueProject/quizloFrontend/src/router/index.js
+++ VueProject/quizloFrontend/src/router/index.js
@@ -1,17 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import QuizzesView from '../views/QuizzesView.vue'
-import Login from '../views/Login.vue' // Import the Login component
-import Register from '../views/Register.vue' // Import the Register component
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 import AboutUs from '../views/AboutUs.vue'
 import QuizView from "@/views/QuizView.vue";
-import PostQuiz from "@/views/PostQuiz.vue"; // Import the AboutUs component
+import PostQuiz from "@/views/PostQuiz.vue";
 import HomePage from "@/views/HomePage.vue";
 import DeleteQuiz from "@/views/DeleteQuiz.vue";
 import PutQuiz from "@/views/PutQuiz.vue";
 import UsersList from "@/views/UsersList.vue";
-import {useAuthStore} from "@/stores/users.js";
-
-
+import { useAuthStore } from "@/stores/users.js";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,12 +25,12 @@ const router = createRouter({
             component: QuizzesView
         },
         {
-            path: '/login', // Add a route for the login page
+            path: '/login',
             name: 'login',
             component: Login
         },
         {
-            path: '/register', // Add a route for the register page
+            path: '/register',
             name: 'register',
             component: Register
         },
@@ -44,50 +42,50 @@ const router = createRouter({
         {
             path: '/quiz/:id',
             name: 'quizId',
-            component: QuizView
-
+            component: QuizView,
+            meta: { requiresLoggedin: true }
         },
         {
             path: '/postQuiz',
-            name:'postQuiz',
-            component:PostQuiz,
+            name: 'postQuiz',
+            component: PostQuiz,
             meta: { requiresAdmin: true }
         },
-        // {
-        //     path: '/deleteQuiz',
-        //     name:'deleteQuiz',
-        //     component:DeleteQuiz
-        // },
         {
             path: '/updateQuiz/:id',
-            name:'updateQuiz',
-            component:PutQuiz,
+            name: 'updateQuiz',
+            component: PutQuiz,
             meta: { requiresAdmin: true }
         },
         {
             path: '/users',
-            name:'users',
-            component:UsersList,
+            name: 'users',
+            component: UsersList,
             meta: { requiresAdmin: true }
         }
-
     ]
 })
 
 router.beforeEach((to, from, next) => {
     const authStore = useAuthStore();
-    // Check if the route requires Admin role
-    if (to.matched.some(record => record.meta.requiresAdmin)) {
-        // Check if the user's role is not 'Admin'
-        if (authStore.userRole !== 'Admin') {
-            // Redirect to the login page
+
+    // Check if the route requires to be logged in
+    if (to.matched.some(record => record.meta.requiresLoggedin)) {
+        if (!authStore.isAuthenticated) {
             next({ name: 'login' });
         } else {
-            // Otherwise, proceed with the route transition
+            next();
+        }
+    }
+
+    // Check if the route requires Admin role
+    else if (to.matched.some(record => record.meta.requiresAdmin)) {
+        if (authStore.userRole !== 'Admin') {
+            next({ name: 'login' });
+        } else {
             next();
         }
     } else {
-        // If the route does not require Admin role, proceed with the route transition
         next();
     }
 });
